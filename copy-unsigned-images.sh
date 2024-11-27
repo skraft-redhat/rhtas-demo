aaac3935789e70142404ed804a0c7182e1dd1081
skraft-redhat <skraft@redhat.com>
2024-11-27 21:46:39 +0100

    Enhancing copy-unsigned

--- copy-unsigned-images.sh
+++ copy-unsigned-images.sh
@@ -3,7 +3,7 @@
 export QUAY_URL=quay.io
 export QUAY_ORGANIZATION=stephan_kraft
 
-echo 'Repo set to $QUAY_URL/$QUAY_ORGANIZATION
+echo 'Repo set to $QUAY_URL/$QUAY_ORGANIZATION'
 
 skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:4d83750a3b60e625c17f679a6406b0ba4126267b16c1cf8f316ea76b24ef99b4 docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:9.5
 skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:7b1bef263bc29e04215b0893b3a8d3b5b8148c9fbcdff755d7df7c1c0e9b5d59 docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:1-79
