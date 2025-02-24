4c7c30fd9cc18c7a15d22ffff7b10b11c3988123
user1 <user1@demo.redhat.com>
2025-02-24 14:46:19 +0100

    changing repos

--- copy-unsigned-images.sh
+++ copy-unsigned-images.sh
@@ -5,6 +5,8 @@ export QUAY_ORGANIZATION=stephan_kraft
 
 echo "Repo set to $QUAY_URL/$QUAY_ORGANIZATION"
 
-skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:4d83750a3b60e625c17f679a6406b0ba4126267b16c1cf8f316ea76b24ef99b4 docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:9.5
-skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:7b1bef263bc29e04215b0893b3a8d3b5b8148c9fbcdff755d7df7c1c0e9b5d59 docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:1-79
-skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:7842d2e458038d2eb54068264c94a1f413d1114d1c4f717f19777f91d56b1a7a docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:1-69
+skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:4d83750a3b60e625c17f679a6406b0ba4126267b16c1cf8f316ea76b24ef99b4 docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:keyful_wo_tlog
+skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:7b1bef263bc29e04215b0893b3a8d3b5b8148c9fbcdff755d7df7c1c0e9b5d59 docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:keyful_with_public_tlog
+skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:7842d2e458038d2eb54068264c94a1f413d1114d1c4f717f19777f91d56b1a7a docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:keyless_with_public_tlog
+skopeo copy docker://registry.access.redhat.com/ubi9/nginx-122@sha256:7842d2e458038d2eb54068264c94a1f413d1114d1c4f717f19777f91d56b1a7a docker://$QUAY_URL/$QUAY_ORGANIZATION/nginx:keyless_with_private_tlog
+
